4e64292f2beb2d7d4f71b7ab927e8903a98ec1ad
近藤 晴輝 <65643010+HarukiKondo@users.noreply.github.com>
2025-11-06 04:20:29 +0000

    追記

--- coinflip/frontend/src/App.tsx
+++ coinflip/frontend/src/App.tsx
@@ -1,39 +1,66 @@
-import { ConnectButton } from "@mysten/dapp-kit";
-import { Box, Container, Flex, Heading } from "@radix-ui/themes";
-import { WalletStatus } from "./WalletStatus";
+import { ConnectButton, useCurrentAccount } from '@mysten/dapp-kit';
+import { InfoCircledIcon } from '@radix-ui/react-icons';
+import { Box, Callout, Container, Flex, Grid, Heading } from '@radix-ui/themes';
 
+import { HOUSECAP_ID, PACKAGE_ID } from './constants';
+import { HouseSesh } from './containers/House/HouseSesh';
+import { PlayerSesh } from './containers/Player/PlayerSesh';
+
+/**
+ * Appコンポーネント
+ * @returns 
+ */
 function App() {
-	return (
-		<>
-			<Flex
-				position="sticky"
-				px="4"
-				py="2"
-				justify="between"
-				style={{
-					borderBottom: "1px solid var(--gray-a2)",
-				}}
-			>
-				<Box>
-					<Heading>dApp Starter Template</Heading>
-				</Box>
+  const account = useCurrentAccount();
+  return (
+    <>
+      <Flex
+        position="sticky"
+        px="4"
+        py="2"
+        justify="between"
+        style={{
+          borderBottom: '1px solid var(--gray-a2)',
+        }}
+      >
+        <Box>
+          <Heading>Satoshi Coin Flip Single Player</Heading>
+        </Box>
 
-				<Box>
-					<ConnectButton />
-				</Box>
-			</Flex>
-			<Container>
-				<Container
-					mt="5"
-					pt="2"
-					px="4"
-					style={{ background: "var(--gray-a2)", minHeight: 500 }}
-				>
-					<WalletStatus />
-				</Container>
-			</Container>
-		</>
-	);
+        <Box>
+          <ConnectButton />
+        </Box>
+      </Flex>
+      <Container>
+        <Heading size="4" m={'2'}>
+          Package ID: {PACKAGE_ID}
+        </Heading>
+        <Heading size="4" m={'2'}>
+          HouseCap ID: {HOUSECAP_ID}
+        </Heading>
+
+        <Callout.Root mb="2">
+          <Callout.Icon>
+            <InfoCircledIcon />
+          </Callout.Icon>
+          <Callout.Text>
+            You need to connect to wallet that publish the smart contract package
+          </Callout.Text>
+        </Callout.Root>
+
+        {!account ? (
+          <Heading size="4" align="center">
+            Please connect wallet to continue
+          </Heading>
+        ) : (
+          <Grid columns="2" gap={'3'} width={'auto'}>
+            <PlayerSesh />
+            <HouseSesh />
+          </Grid>
+        )}
+      </Container>
+    </>
+  );
 }
 
 export default App;
